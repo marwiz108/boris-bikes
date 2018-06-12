5fc47730d62a3c7ebaefe84321f7316734bed9ee
Marwa Elali <marwaelali@gmail.com>
2018-06-12 14:36:30 +0100

    raising exceptions

--- README.md
+++ README.md
@@ -26,4 +26,4 @@ I want to see a bike that has been docked
 As a member of the public,
 So that I am not confused and charged unnecessarily,
 I'd like docking stations not to release bikes when there are none available
-zzz
+```
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,16 +3,26 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
+  describe '#release_bike' do
+    it 'shows docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when docking station is empty' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
   it 'releases a working bike' do
-    bike = subject.release_bike
+    bike = Bike.new
+    subject.dock(bike)
+    subject.release_bike
     expect(bike).to be_working
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it 'shows docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
-  end
+
 end

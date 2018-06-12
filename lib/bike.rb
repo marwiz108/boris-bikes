f339d8da13b5d8613feebc426ced31dc2486c5ff
Marwa Elali <marwaelali@gmail.com>
2018-06-12 14:11:04 +0100

    create DockingStation and Bike classes, which release a working bike

---
+++ lib/bike.rb
@@ -0,0 +1,7 @@
+require_relative 'bike'
+
+class Bike
+  def working?
+    true
+  end
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,10 @@
+require 'docking_station'
+
 describe DockingStation do
-  #it "releases bike" do
-    #expect(DockingStation(bike)).to eq "bike"
-  #end
+  it { is_expected.to respond_to :release_bike }
+
+  it 'releases a working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
